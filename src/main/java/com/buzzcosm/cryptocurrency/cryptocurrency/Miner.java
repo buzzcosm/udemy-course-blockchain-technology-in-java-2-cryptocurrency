9b46d9cbc334d74e7d3826ed2db704506f67eccf
Jeff_Buzz <97390468+buzzcosm@users.noreply.github.com>
2025-05-06 23:57:53 +0200

    improve miner class

--- src/main/java/com/buzzcosm/cryptocurrency/cryptocurrency/Miner.java
+++ src/main/java/com/buzzcosm/cryptocurrency/cryptocurrency/Miner.java
@@ -3,13 +3,17 @@ package com.buzzcosm.cryptocurrency.cryptocurrency;
 import com.buzzcosm.cryptocurrency.blockchain.Block;
 import com.buzzcosm.cryptocurrency.blockchain.Blockchain;
 import com.buzzcosm.cryptocurrency.constants.Constants;
+import lombok.Getter;
+import lombok.extern.log4j.Log4j2;
 
+@Log4j2
+@Getter
 public class Miner {
 
+    // every miner gets 6.25 BTC after the mining operation
     private double reward;
 
     public void mine(Block block, Blockchain blockchain) {
-
         // it takes some time to find the valid hash
         // This is a proof of work (PoW)
         while (!isGoldenHash(block)) {
@@ -17,12 +21,11 @@ public class Miner {
             block.generateHash();
         }
 
-        System.out.println(block + " has just mined...");
-        System.out.println("Hash is: " + block.getHash());
+        log.info( "{} has just mined...", block);
+        log.info("Hash is: {}", block.getHash());
 
         blockchain.addBlock(block);
         reward += Constants.REWARD;
-
     }
 
     private boolean isGoldenHash(Block block) {
@@ -30,7 +33,4 @@ public class Miner {
         return block.getHash().substring(0, Constants.DIFFICULTY).equals(leadingZeros);
     }
 
-    public double getReward() {
-        return reward;
-    }
 }
